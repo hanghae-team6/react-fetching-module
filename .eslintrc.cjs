e08a8fcf5ddc06cc249daf9cdf41633fdc23ac86
JungMinSeung <seuinggg@gmail.com>
2024-04-11 15:41:26 +0900

    feat: prettier 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
