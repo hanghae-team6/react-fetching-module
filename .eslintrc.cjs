cb07517307dcaa1fb64d2e69a48175704ec0d16d
JungMinSeung <seuinggg@gmail.com>
2024-04-11 15:42:25 +0900

    fix: eslint 플러그인 추가 - plugin:@typescript-eslint/recommended-requiring-type-checking (타입 안정성 강화)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,12 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
